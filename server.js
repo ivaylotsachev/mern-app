5da110fd9bded48c41881f43ac64326b51731ab0
ivaylotsachev <itsa4ev@gmail.com>
2023-05-13 13:54:38 +0300

    register and validation

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
+
 /* init app */
 const app = express();
+
 // connect database
 connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('API Running');
