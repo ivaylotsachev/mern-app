c1b66c99c988923d6261e40a42203da7903829fc
ivaylotsachev <itsa4ev@gmail.com>
2023-05-13 12:54:05 +0300

    base routes setup

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const app = express();
 // connect database
 connectDB();
 
+app.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started on: ${PORT}`);
-});
-
-app.get('/', (req, res) => {
-    res.send('API Running');
 });
